995123483d13df0cd2fa8a7445f8599868fe1968
Bastien MENNESSON <bmennesson@linagora.com>
2016-05-03 14:12:55 +0200

    Added alpine-iso sources

---
+++ alpine-xen.conf.mk
@@ -0,0 +1,7 @@
+ALPINE_NAME     := alpine-xen
+MODLOOP_EXTRA   :=
+KERNEL_FLAVOR   := grsec
+# Example of serial console boot and Dom0 memory limit
+#XEN_PARAMS	:= com1=115200,8n1 console=com1 dom0_mem=512M
+#BOOT_OPTS	:= console=hvc0
+BOOT_OPTS	:= nomodeset
---
+++ alpine.conf.mk
@@ -0,0 +1,4 @@
+ALPINE_NAME     := alpine
+MODLOOP_EXTRA   :=
+KERNEL_FLAVOR   := grsec
+
