670f158b4bc58d70eaa51a6478e02b83532b1d03
Bastien MENNESSON <bmennesson@linagora.com>
2016-05-11 16:06:00 +0200

    Updated conf and packages to match KVM usage

--- alpine.conf.mk
+++ alpine.conf.mk
@@ -1,4 +1,6 @@
-ALPINE_NAME     := alpine
+ALPINE_NAME     := alpine-kvm
 MODLOOP_EXTRA   :=
 KERNEL_FLAVOR   := grsec
+KERNEL_FLAVOR_DEFAULT := grsec
+APKOVL := http://127.0.0.1/alpine-kvm.apkovl.tar.gz
 
